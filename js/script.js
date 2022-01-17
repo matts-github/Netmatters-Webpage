5ccf708d85fe186c62eaa5aa452e29afcb0a79a7
matts-github <mattwoodhouse@live.co.uk>
2022-01-17 16:24:03 +0000

    scss and js update

--- js/script.js
+++ js/script.js
@@ -41,28 +41,27 @@ window.onload = () => {
     
 };
 $(document).ready(function () {
-    $(document).on('click', ".lines-button", function () {
-        $('#overlay').show();
-        $('.lines-button').addClass('close');
-    });
-    $(document).on('click', ".lines-button.close", function () {
-        $('#overlay').hide();
-        $('.lines-button').removeClass('close');
-    });
-});
 const menu_btn = document.querySelector('.lines-button');
 const mobile_menu = document.querySelector('.main');
 const dimmed = document.querySelector('.menu-active');
-const shut_menu = document.querySelector('menu-active');
+const shut_menu = document.querySelector('.menu-active');
 
 menu_btn.addEventListener('click', function() {
-    menu_btn.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
-    dimmed.classList.toggle('is-active');
+    toggleMenu();
+    $('#overlay').show();
+    $('.lines-button').addClass('close');
 });
+
 dimmed.addEventListener('click', function(){
-    shut_menu.classList.toggle('menu-active');
+    toggleMenu();
+    $('#overlay').hide();
+    $('.lines-button').removeClass('close');
 })
 
-$('#topbar').scrollupbar();
+function toggleMenu() {
+      menu_btn.classList.toggle('is-active');
+      mobile_menu.classList.toggle('is-active');
+      dimmed.classList.toggle('is-active');
+}
 
+$('#topbar').scrollupbar();
